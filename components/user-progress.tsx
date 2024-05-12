94ae24b007629f928114dc0449223a8907440e50
Raj Kumar kausik <rrajkukausik@gmail.com>
2024-05-12 22:43:12 +0530

    feat: Added userProgress component

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -3,11 +3,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
-// import { courses } from "@/db/schema";
+import { courses } from '@/db/schema';
 
 type UserProgressProps = {
-  //   activeCourse: typeof courses.$inferSelect;
-  activeCourse: { imageSrc: string; title: string };
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
